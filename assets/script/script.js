de5fe27c4898edb0db90f46105c9ed98e01346f3
JoeBosz <joeboz2213@gmail.com>
2022-11-17 21:25:48 -0600

    Add button event listner

---
+++ assets/script/script.js
@@ -0,0 +1,16 @@
+var searchRestaurant=document.getElementById("restaurant-search") 
+var searchEvent=document.getElementById("event-search")
+
+// TODO: get event listeners hooked up
+// TODO: get input values after click
+// TODO: Fire off correct api repsonses depending on which button I clicked 
+// TODO: Display data
+
+searchRestaurant.addEventListener('click', function(ev) {
+    ev.preventDefault()
+    console.log("clicked on searchRestaurant");
+    
+});
+
+
+
