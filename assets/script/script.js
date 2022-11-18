7c69c6754c73f7a90315dadbc7c5c7c4b5b29f7e
Jason Charney <jrcharney@gmail.com>
2022-11-17 21:27:16 -0600

    Let me try my own branch

---
+++ assets/script/script.js
@@ -0,0 +1,13 @@
+/* File: assets/script */
+
+let city  = document.getElementById("city");
+let state = document.getElementById("state");
+let zip   = document.getElementById("zip");
+
+let restaurantQuery  = document.getElementById("restaurant-query");
+//let restaurantSearch = document.getElementById("restaurant-search");
+
+let eventQuery  = document.getElementById("event-query");
+//let eventSearch = document.getElementById("event-search");
+
+/* TODO: WHEN do we want to do these things? */
