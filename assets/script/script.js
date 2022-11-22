f5456f3b2c105cbde13b12095af754deea45cc89
Jason Charney <jrcharney@gmail.com>
2022-11-22 15:40:44 -0600

    Code refactoring and a new page for search results

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,40 +1,43 @@
 // TODO: get event listeners hooked up
 // TODO: get input values after click
-// TODO: Fire off correct api repsonses depending on which button I clicked 
+// TODO: Fire off correct api responses depending on which button I clicked 
 // TODO: Display data
 
-var zipInputEl = document.querySelector("#zip");
-var zipcode = [];
 
+const zipInputEl = document.querySelector("#zip");
+let zipcode = [];
+
+// TODO: Should this function be asynchronous? --J.C.
 function getRestaurant() {
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1d4d2afd06msh8b774c1f4d35aa9p188f7djsn4373734bdfde',
-		'X-RapidAPI-Host': 'restaurants-near-me-usa.p.rapidapi.com'
-	}
-};
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '1d4d2afd06msh8b774c1f4d35aa9p188f7djsn4373734bdfde',
+            'X-RapidAPI-Host': 'restaurants-near-me-usa.p.rapidapi.com'
+        }
+    };
 
-fetch('https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode/63125/0', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+    fetch('https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode/63125/0', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
 };
  
-getRestaurant();
+getRestaurant();    // TODO: Why do we need to run this the first time?
 
-var formSumbitHandler = function(event){
+const saveSearch = function(){
+    localStorage.setItem("zipcode", zipcode);
+};
+
+const formSumbitHandler = function(event){
     event.preventDefault();
-    var zip = zipInputEl.value.trim();
-    if(zip){
+    let zip = zipInputEl.value.trim();
+    if(/^\d{5}$/.test(zip)){      // Using a regular expression to validate tha our zipcode is a zip code
+        // TODO: Should we store our zipcode value in localStorage?
         getRestaurant(zip);
         zipcode.unshift({zip});
         zipInputEl.value = "";
     } else{
-        alert("Please enter a zipcode");
+        alert("Please enter a zipcode");    // TODO: display some text near the submision form instead of using this. I'll look into it later.
     };
-};
-
-var saveSearch = function(){
-    localStorage.setItem("zipcode", zipcode);
 };
