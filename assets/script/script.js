675f606c9cfb742378e650faf9c32d59d2a22f35
Jason Charney <jrcharney@gmail.com>
2022-11-28 04:25:41 -0600

    Do not touch anything. BRB after sleep.

--- assets/script/script.js
+++ assets/script/script.js
@@ -19,16 +19,24 @@ map.addLayer(layer);
 let marker = new L.Marker(mapOptions.center);
 marker.addTo(map);
 
+const cityInput = document.querySelector("#city");
+const stateInput = document.querySelector("#state");
 // Somebody deleted this. I put it back. Have a better name for it too. --JC
 const zipInput = document.querySelector("#zip");
 
 // Note: Somebody forgot to add an argument. I fixed it. --JC
+// TODO: What if we wanted to do a save search based on city and state or geocoordinates?
+// We might want to redo this function
 const saveSearch = function(zipcode){
     localStorage.setItem("zipcode", zipcode);
 };
 
+// TODO: I think we should get rid of this function
 const formSumbitHandler = function(event){
     event.preventDefault();
+    let city = cityInput.value.trim();
+    let state = stateInput.value;   // This doesn't need a trim
+
     let zip = zipInput.value.trim();
     if(/^\d{5}$/.test(zip)){      // Using a regular expression to validate that our zipcode is a zip code
         // TODO: See if we can store city and state in local storage as an alternate to local storage. Yelp API could help with that.
@@ -39,7 +47,7 @@ const formSumbitHandler = function(event){
         zipcode.unshift({zip});     // TODO: What was this for?
         zipInputEl.value = "";
         */
-    } else{
+    }else{
         alert("Please enter a zipcode");    // TODO: display some text near the submission form instead of using this. I'll look into it later.
     };
 };
@@ -64,6 +72,15 @@ restaurantSearch.disabled = true;   // If our zipInputEl and restaurantQuery are
  * 
  */
 
+cityInput.addEventListener("keyup", (ev) => {
+    ev.preventDefault();
+});
+
+// TODO: Make sure stateInput is not the first item on the list
+/*
+stateInput.addEventListener()
+*/
+
 zipInput.addEventListener("keyup", (ev) => {
     ev.preventDefault();    // TODO: Remove this later when we use our form
     restaurantSearch.disabled = (ev.target.value.length === 0 || restaurantQuery.value.length === 0);
@@ -81,7 +98,50 @@ restaurantQuery.addEventListener("keyup", (ev) => {
     }
 });
 
+/*
 restaurantSearch.addEventListener("click", (ev) => {
     ev.preventDefault();    // TODO: Remove this later when we use our form
     // TODO: Run our search
+});
+*/
+
+// You guys remember that we can't use the Yelp API on the client side to do queries, right? WHOOPS!
+
+const findRestaurants = document.querySelector("#findRestaurants");     // get our form
+
+function findTheRestaurants(){
+    const data = new FormData(findRestaurants);     // Get our data from the form, the easy way
+    localStorage.setItem("lastSearch",data);      // WE should use this instead.
+    // TODO: later get this search with lastSearch
+    // TODO: Queue a list of the last 10 searches
+    // TODO: Purge those searches
+    console.log(JSON.stringify([...data.entries()]));
+
+    // We need ot use the cors-anywhere Heroku App to bypass the Yelp API
+
+    let queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search";
+
+    // TODO: Add yelp key to our data entries
+
+    // What's going on here?
+    // We are converting or data.entries() to an array
+    // The array is filtered to remove any empty values
+    // The remaining values are mapped into a key and a URL friendly string
+    // Those strings are joined together with ampersands
+    let urlParams = [...data.entries()]
+                        .filter(([k,v]) => v !== "")
+                        .map(([k,v]) => `${k}=${encodeURIComponent(v)}`)
+                        .join("&");
+    console.log(urlParams);
+
+    /*
+    let options = (
+        method: 'get',
+    );
+    */
+}
+
+restaurantSearch.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    findTheRestaurants();    
 });
